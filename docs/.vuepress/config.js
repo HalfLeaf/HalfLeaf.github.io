b63f06e4b32b8e1a54f7a6eb7aa1cb39325d1ea9
半片叶 <34050019+HalfLeaf@users.noreply.github.com>
2020-06-22 20:42:12 +0800

    更新界面

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,10 +20,6 @@ module.exports = resolve({
       nextLinks:true,          // 所有页面的 下一篇 链接
       nextLinks:true,         // 所有页面的 上一篇 链接
       SEO: false,            // SEO 搜索
-      // Markdown 增强
-      markdown:{
-        enableAll:true
-      },
       themeColor: {
         blue: '#409eff',
         red: '#f56c6c',
@@ -49,6 +45,7 @@ module.exports = resolve({
             },
             { text: 'Solution', link: '/solution/', icon: 'project' },
             { text: 'Collection', link: '/collection/', icon: 'project' },
+            { text: 'TimeLine', link: '/timeline/', icon: '/timeline.ico' },
           ]
         }
       },
@@ -61,95 +58,19 @@ module.exports = resolve({
             { text: 'vue', link: '/vue/', icon: 'infofill' },
             { text: '后端技能', link: '/backend/', icon: 'markdown' },
             { text: '前端技术', link: '/fontend/', icon: 'markdown' },
-            { text: '前沿技术', link: '/advanced/', icon: 'markdown'},
+            { text: '前沿技术', link: '/advanced/', icon: 'icon-markdown'},
           ]
         },
         { text: '踩坑随记', link: '/solution/', icon: 'project' },
         { text: '收藏', link: '/collection/', icon: 'project' },
+        { text: '时间轴', link: '/timeline/', icon: 'icon-project' },
       ],
       // 侧边栏
       sidebar:[],
-      blog:false
+      blog:false,
+      // Markdown 增强
+      markdown: {
+        enableAll: true,
+      },
     }
 })
-
-
-
-/* 
-
-     isTimeLine: true, // 时间轴
-     serviceWorker: true,
-     base: '/', // 这是部署到github相关的配置  使用'/'时
-                //部署到 https://<USERNAME>.github.io  USERNAME=你的用户名
-      markdown: {
-        lineNumbers: true // 代码块显示行号
-      },
-      themeConfig: {
-        sidebar: 'auto',
-        sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
-        lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
-        
-       //导航栏配置
-        nav: [
-            { text: '首页', link: '/'},
-            { text: 'Python', link: '/md/python/python.md'},
-            {
-              text: '前端',
-              items: [
-                  { text: 'vue', link: '/md/vue/vue.md' },
-                  { text: 'node.js', link: '/md/nodejs/nodejs.md' },
-                  { text: 'Javascript', link: '/md/javascript/javascript.md' },
-              ]
-            },
-            {
-                text: '后端',
-                items: [
-                    { text: 'java', link: '/md/java/java.md' },
-                    { text: '数据库', link: '/md/database/db.md' },
-                    { text: '深度学习', link: '/md/deeplearn/deeplearn.md' },
-                ]
-            }, 
-            { text: '收藏', link: '/gather/gather.md' , icon: '/gather.ico'},
-            { text: '时间轴', link: '/timeline/', icon: '/timeline.ico' },
-            {text:'关于', link:'/guide/guide.md'},
-        ],
-        displayAllHeaders: true, // 默认值：false
-        activeHeaderLinks: true, // 默认值：true
-        //侧边栏配置
-        sidebar: {
-            '/': [
-                // 侧边栏在 /python/ 目录上
-                {
-                    title: 'python',
-                    collapsable: true, // 不可折叠
-                    children: [
-                      '/md/python/python.md',
-                      
-                    ]
-                },
-                // 侧边栏在 /vue/ 目录上
-                {
-                  title: 'Vue',
-                  collapsable: true, // 不可折叠
-                  children: [
-                    '/md/vue/vue.md'
-                  ]
-                },
-                {
-                  title: '链接',
-                  collapsable: false, // 不可折叠
-                  children: [
-                    { text: 'Github', link: 'https://github.com/HalfLeaf', icon: '/github.ico' },
-                    { text: 'Gitee', link: 'https://github.com/HalfLeaf', icon: '/gitee.ico' },
-                    { text: '博客园', link: 'https://www.cnblogs.com/halfleaf', icon: '/bokeyuan.ico' },
-                  ]
-              },
-            ]
-        }        
-    },
-             
-    algolia: {
-        apiKey: '<API_KEY>',
-        indexName: '<INDEX_NAME>'
-        }
-*/
